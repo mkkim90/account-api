4d958bbafe9ca407bf117b702bf234b433600044
mkkim90 <mkzzang0928@gmail.com>
2021-06-21 11:05:34 +0900

    init : 서버세팅

---
+++ src/main/java/com/kpsec/test/infra/InitData.java
@@ -0,0 +1,38 @@
+package com.kpsec.test.infra;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import com.kpsec.test.model.entity.Account;
+import com.kpsec.test.repository.AccountRepository;
+
+@Component
+public class InitData {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @PostConstruct
+    private void initAccount() throws IOException {
+        if (accountRepository.count() == 0) {
+            Resource resource = new ClassPathResource("계좌정보.csv");
+            List<Account> accountList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8)
+                    .stream().skip(1).map(line -> {
+                        String[] split = line.split(",");
+                        return Account.builder().accountNo(split[0]).accountName(split[1]).branchCode(split[2])
+                                .build();
+                    }).collect(Collectors.toList());
+            accountRepository.saveAll(accountList);
+        }
+    }
+}
